99c941aae32a4cee281236ae104843cc615fbbad
RomanPrygunkov <103605350+RomanPrygunkov@users.noreply.github.com>
2022-08-11 01:13:03 +0300

    done

---
+++ js.js
@@ -0,0 +1,21 @@
+function getSumOfNumbers(number, type) { 
+    let total = 0
+  
+    for (let i = 0; i <= number; i += 1) { 
+        if (type === "odd" && i % 2 ) {                // нечётные
+             total += i 
+        } else if (type === "even" && i % 2  === 0) {  // чётные
+            total += i 
+        } else if (type === "") {                      // все
+            total += i
+        }  
+    }
+   
+    if (!Number(number) || type !== "odd" && type !== "even" && type !== "") {
+     total = NaN
+    }   
+    
+    return console.log(total);
+}
+
+getSumOfNumbers (10, "");
