0849635dc4d27d6e7e075ce3fefd82f1aa8bbe5d
RomanPrygunkov <103605350+RomanPrygunkov@users.noreply.github.com>
2022-08-11 18:48:39 +0300

    fixed check of number

--- js.js
+++ js.js
@@ -1,21 +1,23 @@
 function getSumOfNumbers(number, type = "odd") { 
     let total = 0
     
+    if (typeof number !== "number" || type !== "odd" && type !== "even" && type !== "") {
+             total = NaN
+
+         }  else {
     for (let i = 0; i <= number; i += 1) { 
-        if (type === "odd" && i % 2 ) {                // нечётные
+       
+        if (type === "odd" && i % 2) {                // нечётные
              total += i 
         } else if (type === "even" && i % 2  === 0) {  // чётные
             total += i 
         } else if (type === "") {                      // все
             total += i
-        }  
+             }      
     }
-   
-    if (!Number(number) || type !== "odd" && type !== "even" && type !== "") {
-     total = NaN
-    }   
-    
-    return console.log(total);
+    }
+  
+    return console.log(total);;
 }
 
 getSumOfNumbers (10, "even");
